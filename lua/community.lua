c61d8d0bf15fd0fbd4e7ff5d56f7da7ff5b4fd2d
Ben <info@bendz.dev>
2024-07-13 10:27:45 +0200

    add flash

--- lua/community.lua
+++ lua/community.lua
@@ -39,5 +39,6 @@ return {
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
 }
